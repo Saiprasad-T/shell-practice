4816caca7fa9e292e0edf51fe94997d1759e648a
SaiThota <thotasaiprasad180@gmail.com>
2026-01-29 11:28:39 +0530

    new concept

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "enter your name"
-read user
-echo "wlecome $user , DevOps journey starts now"
+echo "enter your name and age"
+read name age
+echo "iam $name and my $age and im looking for aunty"
