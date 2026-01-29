f38f735d9050c2e4948ff942e6d8ce3f038652c6
SaiThota <thotasaiprasad180@gmail.com>
2026-01-29 11:53:44 +0530

    new

--- variables.sh
+++ variables.sh
@@ -2,8 +2,8 @@
 
 read -p "enter the env where you want to push the files:" stage
 
-if [ $stage==prod ]; then
+if [ "$stage" = prod ]; then
  echo "be carefull as this the production"
-else
+else 
  echo "safe to proceed"
 fi
