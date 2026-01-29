c41c27b01ddf3cd721908a0d24feae8ffdcdf6c6
SaiThota <thotasaiprasad180@gmail.com>
2026-01-29 14:33:28 +0530

    new

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,17 @@ logs_folder="/var/log/shellscripts/"
 logs_file="/var/log/shellscripts/$0.log"
 
 if [ $userid -ne 0 ]; then
- echo"you are not a root user"
+ echo"you are not a root user" | tee -a $logs_file
  exit 1
 fi
 
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then
- echo "$2 .....Failure"
+ echo "$2 .....Failure" | tee -a $logs_file
  exit 1
 else
- echo "$2....Sucess"
+ echo "$2....Sucess" | tee -a $logs_file
 fi
 }
 
