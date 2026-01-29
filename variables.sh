d3ddaa157de4611709eae59955c767fe20dc03bd
SaiThota <thotasaiprasad180@gmail.com>
2026-01-29 13:12:54 +0530

    new

--- variables.sh
+++ variables.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 userid=$(id -u)
-if [ "$userid" -ne 0]; then
+if [ $userid -ne 0 ]; then
  echo "you are not a root user, please run this script with root user"
+ exit 1
 fi
 
-VALIDATE(){
- if [ $1 -ne 0 ]; then
+VALIDATE() {
+if [ $1 -ne 0 ]; then
   echo "$2 .....failure"
 else
  echo "$2 .....sucess"
+fi
 } 
 
 dnf install nginx -y
