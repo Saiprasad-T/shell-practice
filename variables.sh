60a669ac364df1856c5d35ddad7e6d0549e69ea0
SaiThota <thotasaiprasad180@gmail.com>
2026-01-29 12:04:32 +0530

    new

--- variables.sh
+++ variables.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-read -p "enter the env where you want to push the files:" stage
-
-if [ "$stage" = prod ]; then
- echo "be carefull as this the production"
-else 
- echo "safe to proceed"
+if [ $(id -u) -ne 0 ]; then
+ echo "you are not a root user"
+else
+ echo "you are root user starting the installing"
 fi
