800e599e65355ecb70a2856b8ca2844df06c1c73
SaiThota <thotasaiprasad180@gmail.com>
2026-01-30 23:14:33 +0530

    new

---
+++ using tee.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#checks for the user_id, if user is not super user it fails
+user_id=$(id -u)
+#creating logs folder and logs file to store the logs
+logs_folder="/var/log/shell-scripts/"
+logs_file="/var/log/shell-scripts/$0.log"
+
+if [ $user_id -ne 0 ]; then 
+ echo "please run it as root user..." | tee -a $logs_file
+ exit 1
+fi
+#written a function as this requried for everystep
+verification () {    
+if [ $1 -eq 0 ]; then
+ echo "$2 installation is success" | tee -a $logs_file
+else
+ echo "$2 not installed" | tee -a $logs_file
+fi
+}
+
+#using dnf commanf for installing applications
+dnf install nginx -y  &>> $logs_file 
+verification $? "nginx"
+
+dnf install mysql-server -y &>> $logs_file
+verification $? "mysql-server"
