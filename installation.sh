b0e5f725a7c0dce24741c24dfeedfbdb0ba16f29
SaiThota <thotasaiprasad180@gmail.com>
2026-01-30 20:17:23 +0530

    new

--- installation.sh
+++ installation.sh
@@ -2,14 +2,14 @@
 
 user_id=$(id -u)
 
-if [[ $user_id -ne o ]];
+if [[ $user_id -ne o ]]; then
  echo "please run with root user...."
  exit 1
 fi
 
 dnf install nginx -y
 
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
  echo "installation is succesfull"
  exit 1
 else
