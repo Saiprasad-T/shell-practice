649638d6b73c67d42811172e9919f044252e9aab
SaiThota <thotasaiprasad180@gmail.com>
2026-01-30 22:33:09 +0530

    new

--- newone.sh
+++ newone.sh
@@ -2,12 +2,15 @@
 
 #checks for the user_id, if user is not super user it fails
 user_id=$(id -u)
-if [ $user_id -ne 0 ]; then
+#creating logs folder and logs file to store the logs
+logs_file="/var/log/shell-scripts/$0.log"
+
+if [ $user_id -ne 0 ]; then &>> $log_file
  echo "please run it as root user..."
  exit 1
 fi
 #written a function as this requried for everystep
-verification () {
+verification () {    &>> $log_file
 if [ $1 -eq 0 ]; then
  echo "$2 installation is success"
 else
@@ -16,8 +19,8 @@ fi
 }
 
 #using dnf commanf for installing applications
-dnf install nginx -y
+dnf install nginx -y  &>> $log_file
 verification $? "nginx"
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>> $log_file
 verification $? "mysql-server"
