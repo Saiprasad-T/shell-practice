2e24e1aead5393c0c215820d2e705be6270fa95f
SaiThota <thotasaiprasad180@gmail.com>
2026-01-30 19:39:08 +0530

    new

---
+++ age.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter your age:"
+read $age
+
+if [[ $age -le 18 ]]; then
+ echo "you are a minor"
+elif [[ $age -ge 18 && $age -le 60 ]]; then
+ echo "you are adult...."
+else
+ echo "you are citizen"
+fi
