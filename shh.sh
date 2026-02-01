dafb9057049953cc6e9acb000ee99a85f19d4ac1
SaiThota <thotasaiprasad180@gmail.com>
2026-02-01 16:45:01 +0530

    new

--- shh.sh
+++ shh.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-ami-id="$ami-0220d79f3f480ecf5"
-sg-id="$sg-0f34bb41316585429"
-instance_type="$t3.micro"
+ami_id="ami-0220d79f3f480ecf5"
+sg_id="sg-0f34bb41316585429"
+instance_type="t3.micro"
 
 for instance in $@
 do 
-    aws ec2 run-instances \
-    --image-id $ami-id \
+   INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $ami_id \
     --instance-type $instance_type \
-    --security-group-ids $sg-id \
-    --query 'Instances[0].PrivateIpAddress' \
-    --output text
+    --security-group-ids $sg_id \
+    --query 'Instances[0].InstanceId' \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --output text )
+
+    if [ "$instance" == "frontend" ]; then
+       IP=$(
+        aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text
+       )
+    else
+        IP=$(
+        aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query 'Reservations[].Instances[].PrivateIpAddress' \
+        --output text
+        )
+    fi
+    echo "ip_adress: $IP"
 done
