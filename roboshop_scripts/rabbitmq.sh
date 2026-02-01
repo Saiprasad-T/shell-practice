8cd30b88a7fe269adddf2f400fef250573ac6240
SaiThota <thotasaiprasad180@gmail.com>
2026-02-01 22:41:39 +0530

    new

---
+++ roboshop_scripts/rabbitmq.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDATE $? "copying apk from repo"
+
+dnf install rabbitmq-server -y
+VALIDATE $? "installing rabbit-mq"
+
+systemctl enable rabbitmq-server
+VALIDATE $? "enabling rabbit-mq"
+
+systemctl start rabbitmq-server
+VALIDATE $? "starting rabbit-mq"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
