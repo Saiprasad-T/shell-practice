4abf91473d2d123a83bc2bac1b875d7ad1d4feb4
SaiThota <thotasaiprasad180@gmail.com>
2026-02-01 22:54:24 +0530

    new

---
+++ roboshop_scripts/payment.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf install python3 gcc python3-devel -y
+VALIDATE $? "installing python"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creates app if not"
+
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
+VALIDATE $? "unzipping from s3"
+
+cd /app 
+unzip /tmp/payment.zip
+VALIDATE $? "unzipping payment.zip"
+
+pip3 install -r requirements.txt
+VALIDATE $? "installing dependencies"
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "configuring systemd"
+
+systemctl daemon-reload
+VALIDATE $? "reloading"
+
+systemctl enable payment 
+VALIDATE $? "enabling payment service"
+
+systemctl start payment
+VALIDATE $? "start payment service"
