4f430d0a04791de2fd5e2c84b280d00ace0e85d4
SaiThota <thotasaiprasad180@gmail.com>
2026-02-04 13:02:42 +0530

    new commit

--- final-roboshopscripts/shipping.sh
+++ final-roboshopscripts/shipping.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 SCRIPT_DIR=$PWD
-MYSQL_HOST="mysqld.devopswiththota.online"
+MYSQL_HOST=mysqld.devopswiththota.online
 
 R="\e[31m"
 G="\e[32m"
@@ -46,40 +46,40 @@ else
     echo -e "Roboshop user already exist ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app 
+mkdir -p /app &>>$LOGS_FILE
 VALIDATE $? "CREATING /APP DIRECTORY"
 
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOGS_FILE
 VALIDATE $? "DOWNLOADING IT FROM S3"
 
-cd /app 
+cd /app &>>$LOGS_FILE
 VALIDATE $? "MOVING /APP DIRECTORY"
 
-rm -rf /app/*
+rm -rf /app/* &>>$LOGS_FILE
 VALIDATE $? "DELETING PREVIOS CODE FROM /APP FOLDER"
 
-unzip /tmp/shipping.zip
+unzip /tmp/shipping.zip &>>$LOGS_FILE
 VALIDATE $? "UNZIPPING INTO /APP FOLDER"
 
-mvn clean package 
+mvn clean package &>>$LOGS_FILE
 VALIDATE $? "INSTALLING DEPENDENCIES"
 
-mv target/shipping-1.0.jar shipping.jar 
+mv target/shipping-1.0.jar shipping.jar &>>$LOGS_FILE
 VALIDATE $? "MOVING 1.0 JAR to .JAR"
 
-cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service &>>$LOGS_FILE
 VALIDATE $? "COPYING .SERVICE FILE INTO SYSTEMD SERVICES"
 
-systemctl daemon-reload
+systemctl daemon-reload &>>$LOGS_FILE
 VALIDATE $? "RELOADING DAEMON"
 
-systemctl enable shipping 
+systemctl enable shipping &>>$LOGS_FILE
 VALIDATE $? "ENABLING SHIPPING"
 
-systemctl start shipping
+systemctl start shipping &>>$LOGS_FILE
 VALIDATE $? "STARTING SHIPPING"
 
-dnf install mysql -y 
+dnf install mysql -y &>>$LOGS_FILE
 
 installing_my_sql () {
     dnf list installed mysql &>/dev/null
@@ -107,6 +107,6 @@ else
 fi
 
 
-systemctl restart shipping
+systemctl restart shipping &>>$LOGS_FILE
 VALIDATE $? "RESTARTING SHIPPING"
 
